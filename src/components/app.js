238de7869c2e1396e2889fa3ae72a613371094c0
Waterburner <Waterburner@users.noreply.github.com>
2021-08-18 16:54:28 -0500

    created home.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Home from "./home";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <Home />
+            </div>
+        );
+    }
 }
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Gilberto Gallery Home</h1>
+            </div>
+        );
+    }
+}
