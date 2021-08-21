70a2b0c716da0b317175112879ffe9e9d4757d24
Waterburner <Waterburner@users.noreply.github.com>
2021-08-21 00:46:26 -0500

    Changed bg img for navbar. Styled "welcome section" at homepage. Created @mixin and adjusted colors.

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -14,48 +14,52 @@ const NavigationComponent = (props) => {
     };
 
     return (
-        <div className="navigation-wrapper">
-            <div className="navigation">
-                <div className="left">
-                    {/* <div className="left">Home</div> */}
-                    <NavLink exact to="/" className="nav-link">
-                        <div className="navbar-link">Home</div>
-                    </NavLink>
-                </div>
-
-                <div className="center">
-                    <div className="logo">
-                        {/* Hard refresh when click on logo */}
-                        <a className="navbar-link" href="#">
-                            Logo
-                        </a>
+        <div className="navigation-container">
+            <div className="navigation-wrapper">
+                <div className="navigation">
+                    <div className="left">
+                        {/* <div className="left">Home</div> */}
+                        <NavLink exact to="/" className="nav-link">
+                            <div className="navbar-link">Home</div>
+                        </NavLink>
                     </div>
-                </div>
 
-                <div className="right">
-                    <div className="hamburger-container">
-                        <div className="hamburger navbar-link">===</div>
-                        <div className="hamburger-dropdown-list-container">
-                            <ul className="dropdown-list">
-                                <a href="/gallery">
-                                    <li className="nav-dropdown-item">
-                                        gallery
-                                    </li>
-                                </a>
-                                <a href="/about">
-                                    <li className="nav-dropdown-item">
-                                        about us
-                                    </li>
-                                </a>
-                                <a href="/contact">
-                                    <li className="nav-dropdown-item">
-                                        contacts
-                                    </li>
-                                </a>
-                                <a href="/auth">
-                                    <li className="nav-dropdown-item">login</li>
-                                </a>
-                            </ul>
+                    <div className="center">
+                        <div className="logo">
+                            {/* Hard refresh when click on logo */}
+                            <a className="navbar-link" href="#">
+                                Logo
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="right">
+                        <div className="hamburger-container">
+                            <div className="hamburger navbar-link">===</div>
+                            <div className="hamburger-dropdown-list-container">
+                                <ul className="dropdown-list">
+                                    <a href="/gallery">
+                                        <li className="nav-dropdown-item">
+                                            gallery
+                                        </li>
+                                    </a>
+                                    <a href="/about">
+                                        <li className="nav-dropdown-item">
+                                            about us
+                                        </li>
+                                    </a>
+                                    <a href="/contact">
+                                        <li className="nav-dropdown-item">
+                                            contacts
+                                        </li>
+                                    </a>
+                                    <a href="/auth">
+                                        <li className="nav-dropdown-item">
+                                            login
+                                        </li>
+                                    </a>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -6,11 +6,28 @@ export default class Home extends Component {
             <div>
                 <div className="homepage-wrapper">
                     <div className="homepage">
-                        <div className="heading-section-wrapper">
-                            <div className="heading-section">
+                        <div className="welcome-section-wrapper">
+                            <div className="welcome-section">
                                 <div className="heading-wrapper">
-                                    <h1 className="main-heading">Gilberto</h1>
-                                    <h2 className="main-subheading">gallery</h2>
+                                    <h1 className="main-heading heading">
+                                        <span className="heading-txt">G</span>
+                                        <span className="heading-txt">i</span>
+                                        <span className="heading-txt">l</span>
+                                        <span className="heading-txt">b</span>
+                                        <span className="heading-txt">e</span>
+                                        <span className="heading-txt">r</span>
+                                        <span className="heading-txt">t</span>
+                                        <span className="heading-txt">o</span>
+                                    </h1>
+                                    <h2 className="main-subheading heading">
+                                        <span className="heading-txt">g</span>
+                                        <span className="heading-txt">a</span>
+                                        <span className="heading-txt">l</span>
+                                        <span className="heading-txt">l</span>
+                                        <span className="heading-txt">e</span>
+                                        <span className="heading-txt">r</span>
+                                        <span className="heading-txt">y</span>
+                                    </h2>
                                 </div>
                             </div>
                         </div>
