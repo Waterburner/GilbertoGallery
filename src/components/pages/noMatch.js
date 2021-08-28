1c918a12e41a3d98de1c58f86a3086ad9990cb4b
Waterburner <Waterburner@users.noreply.github.com>
2021-08-28 02:28:56 -0500

    styled noMatch.js component

--- src/components/pages/noMatch.js
+++ src/components/pages/noMatch.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 
 export default function () {
     return (
-        <div>
-            <h1>Ooooops!</h1>
-            <h2>We don't have this page. </h2>
+        <div className="nomatch-wrapper">
+            <div className="nomatch">
+                <h1>Ooooops!</h1>
+                <h2>We don't have this page. </h2>
+            </div>
         </div>
     );
 }
