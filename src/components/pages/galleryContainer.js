d54e872c3000cd0a2dd16bbdc5a90589c9a9d315
Waterburner <Waterburner@users.noreply.github.com>
2021-08-29 22:36:22 -0500

    added assets for gallery_painting and gallery_frames. Created gallery.scss

--- src/components/pages/galleryContainer.js
+++ src/components/pages/galleryContainer.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 
+import frame from "../../../static/assets/imgs/gallery_frames/gold-frame-fancy-transp.png";
+
 export default function () {
     return (
-        <div>
+        <div className="gallery-container">
             <h2>Gallery Container Page</h2>
+            <div className="gallery">gallery</div>
         </div>
     );
 }
